59f8fc21918c1f72fc31f156fd95bfc393587840
Manoj Raghav <manoj.raghav@mobileprogramming.com>
2021-05-03 18:09:01 +0530

    add header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from '../src/components/header/header.component';
 function App() {
   return (
     <div>
+    <Header></Header>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
